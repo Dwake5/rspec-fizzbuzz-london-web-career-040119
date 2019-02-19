5283ea4ccebff6645c3039d3b7796ddcf7d510b1
Danny Wakeling <wakeling101@hotmail.com>
2019-02-19 11:34:46 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,13 +1,13 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 def fizzbuzz(int)
-  if int % 15 == 0 
-    "Fizzbuzz" 
+  if int % 15 == 0
+    "Fizzbuzz"
   end
-  if int % 5 == 0 
-    "Buzz" 
+  if int % 5 == 0
+    "Buzz"
   end
-  if int % 3 == 0 
-    "Fizz" 
+  if int % 3 == 0
+    "Fizz"
   end
 end
